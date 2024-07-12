755780dbb6502cf4d95febc56d7f3ebc7d8ad619
ohmyc <ohmycatile@gmail.com>
2024-07-12 13:42:23 +0300

    added http methods

--- src/http/request/method.rs
+++ src/http/request/method.rs
@@ -4,14 +4,22 @@ use pyo3::prelude::*;
 #[derive(Clone, Debug)]
 pub enum HttpMethod {
     Get,
+    Put,
+    Head,
     Post,
+    Patch,
+    Delete,
 }
 
 impl HttpMethod {
     pub fn try_from<T: AsRef<str>>(value: T) -> Option<HttpMethod> {
         match value.as_ref() {
             "GET" => Some(HttpMethod::Get),
+            "PUT" => Some(HttpMethod::Put),
+            "HEAD" => Some(HttpMethod::Head),
             "POST" => Some(HttpMethod::Post),
+            "PATCH" => Some(HttpMethod::Patch),
+            "DELETE" => Some(HttpMethod::Delete),
             _ => None,
         }
     }
